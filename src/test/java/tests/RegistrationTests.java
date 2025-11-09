2eb3c389c01fbbf91f2139d41ed996ee7688e9ab
Anna Mamedova <Anyut.prost@yandex.ru>
2025-11-09 22:36:01 +0700

    added allure report and fixed tests

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -3,15 +3,27 @@ package tests;
 import io.qameta.allure.junit4.DisplayName;
 import model.User;
 import org.apache.http.HttpStatus;
+import org.junit.After;
 import org.junit.Test;
 import page.AuthPage;
 
 import static org.hamcrest.Matchers.equalTo;
 
-public class RegistrationTests extends BaseTest {
+public class RegistrationTests {
     public static final String REQUIRED_FIELDS_MESSAGE = "Email, password and name are required fields";
     public static final String USER_ALREADY_EXISTS_MESSAGE = "User already exists";
-    private AuthPage authPage = new AuthPage();
+    public String authToken;
+    private final AuthPage authPage = new AuthPage();
+
+    @After
+    public void deleteCreatedUser() {
+        if (this.authToken != null) {
+            authPage.deleteUser(this.authToken)
+                    .then()
+                    .statusCode(HttpStatus.SC_ACCEPTED);
+            this.authToken = null;
+        }
+    }
 
     @Test
     @DisplayName("Success registration")
@@ -22,10 +34,12 @@ public class RegistrationTests extends BaseTest {
                 "Test User"
         );
 
-        authPage.register(newUser)
+        authToken = authPage.register(newUser)
                 .then()
                 .statusCode(HttpStatus.SC_OK)
-                .body("success", equalTo(true));
+                .body("success", equalTo(true))
+                .extract()
+                .path("accessToken");
     }
 
     @Test
@@ -51,9 +65,33 @@ public class RegistrationTests extends BaseTest {
     }
 
     @Test
+    @DisplayName("Register without name")
+    public void registerWithoutName() {
+        User userWithoutEmail = new User("test@example.com", "password123", null);
+
+        authPage.register(userWithoutEmail)
+                .then()
+                .statusCode(HttpStatus.SC_FORBIDDEN)
+                .body("message", equalTo(REQUIRED_FIELDS_MESSAGE));
+    }
+
+    @Test
     @DisplayName("Register with same email")
     public void registerWithExistingEmail() {
-        authPage.register(testUser)
+        User newUser = new User(
+                "test" + System.currentTimeMillis() + "@example.com",
+                "password123",
+                "Test User"
+        );
+
+        authToken = authPage.register(newUser)
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("success", equalTo(true))
+                .extract()
+                .path("accessToken");
+
+        authPage.register(newUser)
                 .then()
                 .statusCode(HttpStatus.SC_FORBIDDEN)
                 .body("message", equalTo(USER_ALREADY_EXISTS_MESSAGE));
